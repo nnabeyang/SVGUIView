313058b63c50689950a8b9a90bb731322fcf3f1b
nnabeyang <nabeyang@gmail.com>
2023-03-21 17:24:55 +0900

    Use monospacedSystemFont if the specified font is not available

--- Sources/SVGUIView/Extensions/Model/Primitives/SVGFont+UIFont.swift
+++ Sources/SVGUIView/Extensions/Model/Primitives/SVGFont+UIFont.swift
@@ -2,7 +2,11 @@ import SVGView
 import UIKit
 
 extension SVGFont {
-    func toUIFont() -> UIFont? {
-        UIFont(name: name, size: size)
+    func toUIFont() -> UIFont {
+        if let font = UIFont(name: name, size: size) {
+            return font
+        }
+
+        return UIFont.monospacedSystemFont(ofSize: size, weight: .regular)
     }
 }
