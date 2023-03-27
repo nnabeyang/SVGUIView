ccf505cfc55cd6f60b6c33573e0c4b52d0b5798b
nnabeyang <nabeyang@gmail.com>
2023-03-28 08:55:58 +0900

    Remove unused variables in `UIBezierPath(roundedRect:, cornerSize:)`

--- Sources/SVGUIView/Extensions/UIBezierPath+Extension.swift
+++ Sources/SVGUIView/Extensions/UIBezierPath+Extension.swift
@@ -3,43 +3,35 @@ import UIKit
 extension UIBezierPath {
     convenience init(roundedRect rect: CGRect, cornerSize: CGSize) {
         self.init()
-        let x = rect.minX
-        let y = rect.minY
+
         let rx = cornerSize.width
         let ry = cornerSize.height
 
-        let x0 = rect.maxX - rx
+        let x1 = rect.minX + rx
         let y1 = rect.minY + ry
+        let x2 = rect.maxX - rx
+        let y2 = rect.maxY - ry
+
         let cw = 4.0 * (sqrt(2.0) - 1.0) * rx / 3.0
         let ch = 4.0 * (sqrt(2.0) - 1.0) * ry / 3.0
 
-        move(to: CGPoint(x: x + rx, y: y))
-        addLine(to: CGPoint(x: x0, y: y))
-        addCurve(to: .init(x: x0 + rx, y: y + ry), controlPoint1: .init(x: x0 + cw, y: y), controlPoint2: .init(x: x0 + rx, y: y1 - ch))
-
-        addLine(to: CGPoint(x: x0 + rx, y: rect.maxY - ry))
-
-        let y2 = rect.maxY - ry
-        addCurve(to: .init(x: x0, y: rect.maxY),
-                 controlPoint1: .init(x: rect.maxX, y: y2 + ch),
-                 controlPoint2: .init(x: x0 + cw, y: rect.maxY))
-
+        move(to: CGPoint(x: x1, y: y1 - ry))
+        addLine(to: CGPoint(x: x2, y: y1 - ry))
+        addCurve(to: .init(x: x2 + rx, y: y1),
+                 controlPoint1: .init(x: x2 + cw, y: y1 - ry),
+                 controlPoint2: .init(x: x2 + rx, y: y1 - ch))
+        addLine(to: CGPoint(x: x2 + rx, y: rect.maxY - ry))
+        addCurve(to: .init(x: x2, y: y2 + ry),
+                 controlPoint1: .init(x: x2 + rx, y: y2 + ch),
+                 controlPoint2: .init(x: x2 + cw, y: y2 + ry))
         addLine(to: CGPoint(x: rect.minX + rx, y: rect.maxY))
-
-        let x3 = x + rx
-        let y3 = rect.maxY - ry
-
-        addCurve(to: .init(x: rect.minX, y: y3),
-                 controlPoint1: .init(x: x3 - cw, y: rect.maxY),
-                 controlPoint2: .init(x: rect.minX, y: y3 + ch))
-
-        addLine(to: CGPoint(x: rect.minX, y: rect.minY + ry))
-
-        let x4 = x + rx
-
-        addCurve(to: .init(x: x4, y: rect.minY),
-                 controlPoint1: .init(x: rect.minX, y: y1 - ch),
-                 controlPoint2: .init(x: x4 - cw, y: rect.minY))
+        addCurve(to: .init(x: x1 - rx, y: y2),
+                 controlPoint1: .init(x: x1 - cw, y: y2 + ry),
+                 controlPoint2: .init(x: x1 - rx, y: y2 + ch))
+        addLine(to: CGPoint(x: x1 - rx, y: y1))
+        addCurve(to: .init(x: x1, y: y1 - ry),
+                 controlPoint1: .init(x: x1 - rx, y: y1 - ch),
+                 controlPoint2: .init(x: x1 - cw, y: y1 - ry))
         close()
     }
 }
