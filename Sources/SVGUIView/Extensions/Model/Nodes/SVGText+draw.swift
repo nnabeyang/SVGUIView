cef265e39cf7c5079b6a0c790f0e33e07acc6f43
nnabeyang <nabeyang@gmail.com>
2023-04-20 08:44:00 +0900

    Fix text transformation

--- Sources/SVGUIView/Extensions/Model/Nodes/SVGText+draw.swift
+++ Sources/SVGUIView/Extensions/Model/Nodes/SVGText+draw.swift
@@ -2,7 +2,7 @@ import SVGView
 import UIKit
 
 extension SVGText {
-    func draw(_: CGAffineTransform, rect: CGRect) {
+    func draw(_ trans: CGAffineTransform, rect: CGRect) {
         var attributes: [CFString: Any] = [:]
 
         if let color = fill as? SVGColor {
@@ -52,14 +52,15 @@ extension SVGText {
         let framesetter = CTFramesetterCreateWithAttributedString(attributedText)
         let frameSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRange(), nil, CGSize(width: CGFloat(Int32.max), height: CGFloat(Int32.max)), nil)
         guard let uiFont = font?.toUIFont() else { return }
+        let combined = trans.concatenating(transform)
         let bounds = CGRect(origin: .init(x: 0, y: 0), size: .init(width: max(rect.width, frameSize.width), height: frameSize.height))
         let frame = CTFramesetterCreateFrame(framesetter, CFRange(), CGPath(rect: bounds, transform: nil), nil)
         context.scaleBy(x: 1.0, y: -1.0)
 
         if case .center = textAnchor {
-            context.translateBy(x: 0, y: -self.transform.ty - bounds.height + uiFont.ascender)
+            context.translateBy(x: 0, y: -combined.ty - bounds.height + uiFont.ascender)
         } else {
-            context.translateBy(x: transform.tx, y: -transform.ty - bounds.height + uiFont.ascender)
+            context.translateBy(x: combined.tx, y: -combined.ty - bounds.height + uiFont.ascender)
         }
         CTFrameDraw(frame, context)
         context.restoreGState()
