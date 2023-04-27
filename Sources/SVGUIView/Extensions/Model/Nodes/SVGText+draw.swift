17367900912f1111c3a7444d35890b5b159dd7b6
nnabeyang <nabeyang@gmail.com>
2023-04-28 08:48:51 +0900

    Modify using UIBezierPath for drawing text so that stroke thickness and fill are available

--- Sources/SVGUIView/Extensions/Model/Nodes/SVGText+draw.swift
+++ Sources/SVGUIView/Extensions/Model/Nodes/SVGText+draw.swift
@@ -2,22 +2,8 @@ import SVGView
 import UIKit
 
 extension SVGText {
-    func draw(rect: CGRect) {
+    func draw(rect _: CGRect) {
         var attributes: [CFString: Any] = [:]
-
-        if let color = fill as? SVGColor {
-            attributes[kCTForegroundColorAttributeName] = color.toUIColor
-        }
-
-        if let stroke = stroke {
-            if let color = stroke.fill as? SVGColor {
-                attributes[kCTStrokeColorAttributeName] = color.toUIColor
-            } else {
-                attributes[kCTStrokeColorAttributeName] = UIColor.clear
-            }
-            attributes[kCTStrokeWidthAttributeName] = stroke.width
-        }
-
         if let font = font?.toUIFont() {
             let descriptor = font.fontDescriptor as CTFontDescriptor
             let ctFont = CTFontCreateWithFontDescriptor(descriptor, 0.0, nil)
@@ -30,16 +16,54 @@ extension SVGText {
         context.saveGState()
         let framesetter = CTFramesetterCreateWithAttributedString(attributedText)
         let frameSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRange(), nil, CGSize(width: CGFloat(Int32.max), height: CGFloat(Int32.max)), nil)
-        guard let uiFont = font?.toUIFont() else { return }
-        let bounds = CGRect(origin: .init(x: 0, y: 0), size: .init(width: max(rect.width, frameSize.width), height: frameSize.height))
-        let frame = CTFramesetterCreateFrame(framesetter, CFRange(), CGPath(rect: bounds, transform: nil), nil)
+
         context.scaleBy(x: 1.0, y: -1.0)
         if case .center = textAnchor {
-            context.translateBy(x: transform.tx - frameSize.width / 2.0, y: -transform.ty + uiFont.descender)
+            context.translateBy(x: transform.tx - frameSize.width / 2.0, y: -transform.ty)
         } else {
-            context.translateBy(x: transform.tx, y: -transform.ty + uiFont.descender)
+            context.translateBy(x: transform.tx, y: -transform.ty)
         }
-        CTFrameDraw(frame, context)
+
+        let letters = CGMutablePath()
+        let line = CTLineCreateWithAttributedString(attributedText)
+        let runs = CTLineGetGlyphRuns(line)
+
+        for i in 0 ..< CFArrayGetCount(runs) {
+            let run = unsafeBitCast(CFArrayGetValueAtIndex(runs, i), to: CTRun.self)
+            let fontPointer = CFDictionaryGetValue(CTRunGetAttributes(run), Unmanaged.passUnretained(kCTFontAttributeName).toOpaque())
+            let runFont = unsafeBitCast(fontPointer, to: CTFont.self)
+
+            for index in 0 ..< CTRunGetGlyphCount(run) {
+                let range = CFRange(location: index, length: 1)
+                var glyph = CGGlyph()
+                var position = CGPoint()
+                CTRunGetGlyphs(run, range, &glyph)
+                CTRunGetPositions(run, range, &position)
+
+                guard let letter = CTFontCreatePathForGlyph(runFont, glyph, nil) else { continue }
+                letters.addPath(letter, transform: CGAffineTransform(translationX: position.x, y: position.y))
+            }
+        }
+
+        let path = UIBezierPath(cgPath: letters)
+        if let color = fill as? SVGColor {
+            color.toUIColor.setFill()
+        }
+        path.fill()
+
+        if let stroke = stroke {
+            if let color = stroke.fill as? SVGColor {
+                color.toUIColor.setStroke()
+            }
+            path.setLineDash(stroke.dashes, count: stroke.dashes.count, phase: stroke.offset)
+            path.lineWidth = stroke.width
+            path.lineCapStyle = stroke.cap
+            path.lineJoinStyle = stroke.join
+            path.miterLimit = stroke.miterLimit
+        } else {
+            path.lineWidth = 0
+        }
+        path.stroke()
         context.restoreGState()
     }
 }
