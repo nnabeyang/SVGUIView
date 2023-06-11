178fa29229f389cb8eb657755f58d589e8737def
nnabeyang <nabeyang@gmail.com>
2023-06-11 13:04:20 +0900

    Add override of intrinsicContentSize in SVGUIView

--- Sources/SVGUIView/SVGUIView.swift
+++ Sources/SVGUIView/SVGUIView.swift
@@ -38,4 +38,8 @@ public class SVGUIView: UIView {
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override public var intrinsicContentSize: CGSize {
+        svg.size
+    }
 }
