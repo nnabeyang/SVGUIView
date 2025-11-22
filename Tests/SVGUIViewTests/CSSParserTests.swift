c346c9e6240a8ac80145fafddce893266b8191d6
noriaki watanabe <nabeyang@gmail.com>
2025-11-22 11:39:03 +0900

    Simplify declaration-list test by removing custom Result type

--- Tests/SVGUIViewTests/CSSParserTests.swift
+++ Tests/SVGUIViewTests/CSSParserTests.swift
@@ -3,50 +3,6 @@ import _CSSParser
 
 @testable import SVGUIView
 
-enum Result<Success, Failure: Error> {
-  case success(Success)
-  case failure(Failure)
-}
-
-extension Result: Encodable where Success: Encodable, Failure: Encodable {
-  func encode(to encoder: any Encoder) throws {
-    switch self {
-    case .success(let value):
-      try value.encode(to: encoder)
-    case .failure(let error):
-      try error.encode(to: encoder)
-    }
-  }
-}
-
-extension Result: Decodable where Success: Decodable, Failure: Decodable {
-  init(from decoder: any Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    if let value = try? container.decode(Success.self) {
-      self = .success(value)
-      return
-    }
-    if let value = try? container.decode(Failure.self) {
-      self = .failure(value)
-      return
-    }
-    throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: ""))
-  }
-}
-
-extension Result: Equatable where Success: Equatable, Failure: Equatable {
-  static func == (lhs: Self, rhs: Self) -> Bool {
-    switch (lhs, rhs) {
-    case (.success(let lhs), .success(let rhs)):
-      return lhs == rhs
-    case (.failure(let lhs), .failure(let rhs)):
-      return lhs == rhs
-    default:
-      return false
-    }
-  }
-}
-
 struct TestData<T: Codable>: Codable {
   let src: String
   let want: T
@@ -82,9 +38,9 @@ final class CSSParserTests: XCTestCase {
 
   func testDeclarationList() throws {
     let json = try String(contentsOf: Bundle.module.url(forResource: "declaration_list", withExtension: "json")!)
-    let tests = try Self.decoder.decode([TestData<Result<CSSDeclaration, CSSParseError>>].self, from: Data(json.utf8))
+    let tests = try Self.decoder.decode([TestData<CSSDeclaration>].self, from: Data(json.utf8))
     for test in tests {
-      let result = parseDeclaration(src: test.src)
+      let result = try parseDeclaration(src: test.src).get()
       XCTAssertEqual(result, test.want)
     }
   }
